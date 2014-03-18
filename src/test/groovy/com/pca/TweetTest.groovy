ba00642218bec31fd3cb61ab3bc7c89d65400590
Mihail Chirita <mihail@Mihails-MacBook-Pro.local>
2014-03-18 19:53:02 -0400

    Added new functionality to be able to filter tweets by hash tag

--- src/test/groovy/com/pca/TweetTest.groovy
+++ src/test/groovy/com/pca/TweetTest.groovy
@@ -7,16 +7,15 @@ class TweetTest extends GroovyTestCase {
         List tweets = [[hashTags: ["filtered"], tweet: "tweet 1"],
                 [hashTags: ["notFiltered"], tweet: "tweet 2"]]
         TwitterClient client = new TwitterClient(tweets:  tweets);
-        assertEquals([[hashTags: ["filtered"], tweet: "tweet 1"]],
+        assertEquals([tweets[0]],
                 client.filterByHashTag("filtered"))
     }
 
-//    public void test_filtersTweetsWithMultipleHashTags(){
-//        List tweets = [[hashTags: ["filtered", "monkey"], tweet: "tweet 1"],
-//                [hashTags: ["notFiltered"], tweet: "tweet 2"],
-//                [hashTags: ]]
-//        TwitterClient client = new TwitterClient(tweets:  tweets);
-//        assertEquals([[hashTags: ["filtered"], tweet: "tweet 1"]],
-//                client.filterByHashTag("filtered"))
-//    }
+    public void test_filtersTweetsWithMultipleHashTagsReturningMultipleTweets(){
+        List tweets = [[hashTags: ["filtered", "monkey"], tweet: "tweet 1"],
+                [hashTags: ["notFiltered"], tweet: "tweet 2"],
+                [hashTags: ["something", "filtered"], tweet: "another tweet"]]
+        TwitterClient client = new TwitterClient(tweets:  tweets);
+        assertEquals([tweets[0], tweets[2]], client.filterByHashTag("filtered"))
+    }
 }
