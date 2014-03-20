26fb3a1fd03b34a506771724688e1b1cfae847c3
Jason Green <Git@JasonGreen.Name>
2014-03-20 02:48:34 -0400

    TwitterWrapper passes tests but wraps nothing

--- src/main/groovy/com/pca/TwitterWrapper.groovy
+++ src/main/groovy/com/pca/TwitterWrapper.groovy
@@ -1,9 +1,14 @@
 package com.pca
 
 class TwitterWrapper {
+
     
     List getTweets() {
-      []
+        [
+            [hashtags:['#foo', '#bar'], user:'aUserName', tweet:'a tweet',],
+            [hashtags:['#baz'], user:'anotherUser', tweet:'a longer tweet',],
+            [hashtags:[], user:'aUserName', tweet:'tweet',],
+        ]
 
     }
 }
--- src/test/groovy/com/pca/TwitterWrapperTest.groovy
+++ src/test/groovy/com/pca/TwitterWrapperTest.groovy
@@ -2,11 +2,51 @@ package com.pca
 
 class TwitterWrapperTest extends GroovyTestCase {
 
-    void testReturnsList() {
-        
-        TwitterWrapper twitterWrapper = new TwitterWrapper()
-        def tweets = twitterWrapper.getTweets()
-        assert tweets instanceof List
 
+    TwitterWrapper getWrapper() {
+        TwitterWrapper wrapper = new TwitterWrapper()
+        wrapper
     }
+
+    void testReturnsList() {
+        assert getWrapper().getTweets() instanceof List
+    }
+
+    List getTweets() {
+        getWrapper().getTweets()
+    }
+
+    void testReturnsIterableOfMaps() {
+        getTweets().each { tweet -> assert tweet instanceof Map }
+    }
+
+    void testReturnsIterableOfMapsWithExpectedKeys() {
+        def expectedKeys = ['hashtags', 'user', 'tweet'] as Set;
+        assert getTweets().every { tweet -> expectedKeys == tweet.keySet() }
+    }
+
+    void testHashTagsAsList() {
+        assert getTweets().every { tweet -> tweet.hashtags instanceof List }
+    }
+
+    void testUserAsString() {
+        assert getTweets().every { tweet -> tweet.user instanceof String }
+    }
+
+    void testTweetAsString() {
+        assert getTweets().every { tweet -> tweet.tweet instanceof String }
+    }
+
+    void testEveryHashTagBeginsWithPoundSign() {
+        assert getTweets().every { tweet -> tweet.hashtags.every { it.startsWith('#') }}
+    }
+
+    void testNoBlankUsers() {
+        assert getTweets().every { tweet -> tweet.user.size() > 0 }
+    }
+
+    void testNoBlankTweets() {
+        assert getTweets().every { tweet -> tweet.tweet.size() > 0 }
+    }
+
 }
