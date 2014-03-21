800bce2328df8add3279a733a64e780c819626fc
Mike Straw <mstraw@columbus.rr.com>
2014-03-20 21:29:46 -0400

    Added WhiteList implementation.  (I thought I already did this!)

--- src/main/groovy/com/pca/WhiteList.groovy
+++ src/main/groovy/com/pca/WhiteList.groovy
@@ -1,7 +1,17 @@
 package com.pca
 
-/**
- * Created by mess on 3/20/14.
- */
-class WhiteList {
+
+class WhiteList
+{
+    private def handleList = ["Elmer", "Buggs", "Jason"]    // the list of twitter handles on the white list
+
+    // determine if a twitter handle is on the white list
+    boolean isHandleInList(String handle)
+    {
+        // Find the handle in the list.  Use a case insensitive search
+        def foundHandle = handleList.find { it.compareToIgnoreCase(handle) == 0 }
+
+        return (foundHandle != null)
+    }
+
 }
--- src/test/groovy/com/pca/WhiteListTest.groovy
+++ src/test/groovy/com/pca/WhiteListTest.groovy
@@ -1,7 +1,27 @@
 package com.pca
 
-/**
- * Created by mess on 3/20/14.
- */
-class WhiteListTest {
+class WhiteListTest extends GroovyTestCase
+{
+    WhiteList whiteList  // white list used by tests
+
+
+    void setUp()
+    {
+        whiteList = new WhiteList()
+    }
+
+    void testHandleIsInList()
+    {
+        assertTrue(whiteList.isHandleInList("Jason"))
+    }
+
+    void testCaseInsensitiveSearch()
+    {
+        assert(whiteList.isHandleInList("jASON"))
+    }
+
+    void testHandleNotInList()
+    {
+        assertFalse(whiteList.isHandleInList("Mike"))
+    }
 }
