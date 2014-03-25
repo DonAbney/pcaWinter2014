a5005a605eafee888e9d51d212dc345f184408ae
Mike Straw <mstraw@columbus.rr.com>
2014-03-25 18:51:49 -0400

    Changes based on code review.

--- src/main/groovy/com/pca/WhiteList.groovy
+++ src/main/groovy/com/pca/WhiteList.groovy
@@ -1,17 +1,14 @@
 package com.pca
 
-
 class WhiteList
 {
-    private def handleList = ["Elmer", "Buggs", "Jason"]    // the list of twitter handles on the white list
+    private def handleList = ["Elmer", "Buggs", "Jason"]
 
-    // determine if a twitter handle is on the white list
     boolean isHandleInList(String handle)
     {
-        // Find the handle in the list.  Use a case insensitive search
         def foundHandle = handleList.find { it.compareToIgnoreCase(handle) == 0 }
 
-        return (foundHandle != null)
+        foundHandle != null
     }
 
 }
