6fba3fa5d6f109617ffd658a37e3dd73e9aad309
Jason Duffy <jasonsduffy@gmail.com>
2014-03-27 19:19:33 -0400

    add Blacklist.isBlackListed

--- src/main/groovy/com/pca/BlackList.groovy
+++ src/main/groovy/com/pca/BlackList.groovy
@@ -1,7 +1,16 @@
 package com.pca
 
-/**
- * Created by andrew on 3/25/14.
- */
 class BlackList {
+
+    Set handles
+
+    boolean isBlackListed(Tweet tweet) {
+        isBlackListedUser(tweet)
+    }
+
+    private boolean isBlackListedUser(Tweet tweet) {
+        handles.find {
+            tweet.handle.equalsIgnoreCase(it)
+        }
+    }
 }
