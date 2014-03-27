099dab1dc3e7b75807f59727d7ce84a02f322b60
doancea <doancea@doancea-Satellite-C655.(none)>
2014-03-27 17:47:34 -0400

    Card-64 Built member of TweetDisplay class to take a single tweet and build a div as a string.  Green

--- src/main/groovy/com/pca/TwitterDisplay.groovy
+++ src/main/groovy/com/pca/TwitterDisplay.groovy
@@ -1,9 +1,22 @@
 package com.pca
+import groovy.xml.MarkupBuilder
 
 
 class TwitterDisplay {
 
+    StringWriter writer = new StringWriter()
+
     public String getPublicTimelineHtml() {
         "<div></div>"
+    }
+
+    protected String tweetToHtmlDiv(Tweet tweet) {
+        def build = new MarkupBuilder(writer)
+
+        build.div {
+                p (tweet.text)
+            }
+
+        writer.toString()
     }
 }
--- src/test/groovy/com/pca/TwitterDisplayTest.groovy
+++ src/test/groovy/com/pca/TwitterDisplayTest.groovy
@@ -3,11 +3,27 @@ package com.pca
 
 class TwitterDisplayTest extends GroovyTestCase
 {
+    TwitterDisplay twitterDisplay = new TwitterDisplay();
+
+    def tweet1 = new Tweet(id: 1, handle: "doancea",
+            text: "This is how Yoda #YOLO's  #OOYL",
+            hashtags: ["YOLO", "OOYL"]
+    )
+
     public void testDisplayPublicTimelineReturnsADiv()
     {
-        TwitterDisplay twitterDisplay = new TwitterDisplay()
         String timeline = twitterDisplay.getPublicTimelineHtml()
         assertTrue(timeline.startsWith("<div>"))
         assertTrue(timeline.endsWith("</div>"))
+    }
+
+    public void testTweetToHtmlDivReturnsADiv() {
+        def tweetDiv = twitterDisplay.tweetToHtmlDiv(tweet1)
+        assertTrue(tweetDiv.startsWith("<div>"))
+        assertTrue(tweetDiv.endsWith("</div>"))
+    }
+
+    public void testTweetToHtmlContainsTweetText() {
+        assertTrue(twitterDisplay.tweetToHtmlDiv(tweet1).contains(tweet1.text))
     }
 }
