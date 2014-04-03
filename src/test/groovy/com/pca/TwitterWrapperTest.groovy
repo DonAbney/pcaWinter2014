ad69a9c706ac7551e99c4813366d4c4be7c1bce5
Aaron Malone <aaronmalone@gmail.com>
2014-04-03 11:44:20 -0400

    Removing tests from TwitterWrapperTest.groovy that test functionality of Tweet class more than TwitterWrapper itself

--- src/test/groovy/com/pca/TwitterWrapperTest.groovy
+++ src/test/groovy/com/pca/TwitterWrapperTest.groovy
@@ -18,20 +18,4 @@ class TwitterWrapperTest extends GroovyTestCase {
     void testReturnsIterableOfTweets() {
         assert getTweets().every { tweet -> tweet instanceof Tweet }
     }
-
-    void testUserAsString() {
-        assert getTweets().every { tweet -> tweet.handle instanceof String }
-    }
-
-    void testTweetAsString() {
-        assert getTweets().every { tweet -> tweet.text instanceof String }
-    }
-
-    void testNoBlankUsers() {
-        assert getTweets().every { tweet -> tweet.handle.size() > 0 }
-    }
-
-    void testNoBlankTweets() {
-        assert getTweets().every { tweet -> tweet.text.size() > 0 }
-    }
 }
