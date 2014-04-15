bd75ab5bc63e38160bcda31ff16fcd4f7536b500
Kyle Smith <kmaismith@gmail.com>
2014-04-15 18:33:03 -0400

    Renamed members to appropriately reflect what they're representing

--- src/main/groovy/com/pca/TwitterProcessor.groovy
+++ src/main/groovy/com/pca/TwitterProcessor.groovy
@@ -1,19 +1,19 @@
 package com.pca
 
 class TwitterProcessor {
-    private Set<String> blackListUsers = new HashSet<String>()
+    private Set<String> blackListHandles = new HashSet<String>()
     private Set<String> blackListWords = new HashSet<String>()
-    private Set<String> whiteListUsers = new HashSet<String>()
+    private Set<String> whiteListHandles = new HashSet<String>()
 
-    void addBlackListUser(String user) {}
-    void addBlackListWord(String user) {}
-    void addWhiteListUser(String user) {}
+    void addBlackListHandle(String handle) {}
+    void addBlackListWord(String word) {}
+    void addWhiteListHandle(String handle) {}
 
-    void removeBlackListUser(String user) {}
-    void removeBlackListWord(String user) {}
-    void removeWhiteListUser(String user) {}
+    void removeBlackListHandle(String handle) {}
+    void removeBlackListWord(String word) {}
+    void removeWhiteListHandle(String handle) {}
 
-    private void setBlackListUsers(Set<String> blackListUsers) {}
+    private void setBlackListHandles(Set<String> blackListHandles) {}
     private void setBlackListWords(Set<String> blackListWords) {}
-    private void setWhiteListUsers(Set<String> whiteListUsers) {}
+    private void setWhiteListHandles(Set<String> whiteListHandles) {}
 }
