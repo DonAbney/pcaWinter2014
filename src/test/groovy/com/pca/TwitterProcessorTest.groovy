64fde62f9289e4508326f66f93124b488e44a9e9
Mike Straw <mstraw@columbus.rr.com>
2014-04-29 19:53:41 -0400

    Refactor of tests for filter processor method in TwitterProcessor

--- src/test/groovy/com/pca/TwitterProcessorTest.groovy
+++ src/test/groovy/com/pca/TwitterProcessorTest.groovy
@@ -4,56 +4,7 @@ import com.pca.test.utils.TweetBuilder
 
 class TwitterProcessorTest extends GroovyTestCase {
 
-    void testTwitterProcessorProcessTweetsPassesWhitelistFilterCorrectly() {
-        TweetBuilder builder = new TweetBuilder()
-        Tweet tweet = builder.buildTweet()
-
-        List<Tweet> tweetList = [tweet]
-
-        WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
-        whitelistedHandleFilter.metaClass.isWhitelisted() { return true}
-
-        TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, new BlacklistedHandleFilter())
-        processor.processTweets(tweetList)
-
-        assert processor.filteredTweets.whiteListedTweets.size() == 1
-    }
-
-    void testTwitterProcessorProcessTweetsFailsWhitelistFilterCorrectly() {
-        TweetBuilder builder = new TweetBuilder()
-        Tweet tweet = builder.buildTweet()
-
-        List<Tweet> tweetList = [tweet]
-
-        WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
-        whitelistedHandleFilter.metaClass.isWhitelisted() { return false}
-
-        TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, new BlacklistedHandleFilter())
-        processor.processTweets(tweetList)
-
-        assert processor.filteredTweets.whiteListedTweets.size() == 0
-    }
-
-    void testTwitterProcessorProcessTweetsMatchesBlacklistHandleFilterCorrectly() {
-        TweetBuilder builder = new TweetBuilder()
-        Tweet tweet = builder.buildTweet()
-
-        List<Tweet> tweetList = [tweet]
-
-        WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
-        whitelistedHandleFilter.metaClass.isWhitelisted() { return false }
-        BlacklistedHandleFilter blacklistedHandleFilter = new BlacklistedHandleFilter()
-        blacklistedHandleFilter.metaClass.isBlacklisted() { return true }
-        BlacklistedWordFilter blacklistedWordFilter = new BlacklistedWordFilter()
-        blacklistedWordFilter.metaClass.isBlacklisted() { return false }
-
-        TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
-        processor.processTweets(tweetList)
-
-        assert processor.filteredTweets.blackListedTweets.size() == 1
-    }
-
-    void testTwitterProcessorProcessTweetsDoNotMatchBlacklistHandleFilterCorrectly() {
+    void testTwitterProcessorFiltersFalseFalseFalse() {
         TweetBuilder builder = new TweetBuilder()
         Tweet tweet = builder.buildTweet()
 
@@ -69,10 +20,13 @@ class TwitterProcessorTest extends GroovyTestCase {
         TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
         processor.processTweets(tweetList)
 
-        assert processor.filteredTweets.blackListedTweets.size() == 0
+        assertFalse processor.filteredTweets.whiteListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.blackListedTweets.contains(tweet)
+        assert processor.filteredTweets.grayListedTweets.contains(tweet)
+
     }
 
-    void testTwitterProcessorProcessTweetsMatchesBlacklistWordFilterCorrectly() {
+    void testTwitterProcessorFiltersFalseFalseTrue() {
         TweetBuilder builder = new TweetBuilder()
         Tweet tweet = builder.buildTweet()
 
@@ -88,10 +42,12 @@ class TwitterProcessorTest extends GroovyTestCase {
         TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
         processor.processTweets(tweetList)
 
-        assert processor.filteredTweets.blackListedTweets.size() == 1
+        assertFalse processor.filteredTweets.whiteListedTweets.contains(tweet)
+        assert processor.filteredTweets.blackListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.grayListedTweets.contains(tweet)
     }
 
-    void testTwitterProcessorProcessTweetsDoesNotMatchBlacklistWordFilterCorrectly() {
+    void testTwitterProcessorFiltersFalseTrueFalse() {
         TweetBuilder builder = new TweetBuilder()
         Tweet tweet = builder.buildTweet()
 
@@ -100,6 +56,48 @@ class TwitterProcessorTest extends GroovyTestCase {
         WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
         whitelistedHandleFilter.metaClass.isWhitelisted() { return false }
         BlacklistedHandleFilter blacklistedHandleFilter = new BlacklistedHandleFilter()
+        blacklistedHandleFilter.metaClass.isBlacklisted() { return true }
+        BlacklistedWordFilter blacklistedWordFilter = new BlacklistedWordFilter()
+        blacklistedWordFilter.metaClass.isBlacklisted() { return false }
+
+        TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
+        processor.processTweets(tweetList)
+
+        assertFalse processor.filteredTweets.whiteListedTweets.contains(tweet)
+        assert processor.filteredTweets.blackListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.grayListedTweets.contains(tweet)
+    }
+
+    void testTwitterProcessorFiltersFalseTrueTrue() {
+        TweetBuilder builder = new TweetBuilder()
+        Tweet tweet = builder.buildTweet()
+
+        List<Tweet> tweetList = [tweet]
+
+        WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
+        whitelistedHandleFilter.metaClass.isWhitelisted() { return false }
+        BlacklistedHandleFilter blacklistedHandleFilter = new BlacklistedHandleFilter()
+        blacklistedHandleFilter.metaClass.isBlacklisted() { return true }
+        BlacklistedWordFilter blacklistedWordFilter = new BlacklistedWordFilter()
+        blacklistedWordFilter.metaClass.isBlacklisted() { return true }
+
+        TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
+        processor.processTweets(tweetList)
+
+        assertFalse processor.filteredTweets.whiteListedTweets.contains(tweet)
+        assert processor.filteredTweets.blackListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.grayListedTweets.contains(tweet)
+    }
+
+    void testTwitterProcessorFiltersTrueFalseFalse() {
+        TweetBuilder builder = new TweetBuilder()
+        Tweet tweet = builder.buildTweet()
+
+        List<Tweet> tweetList = [tweet]
+
+        WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
+        whitelistedHandleFilter.metaClass.isWhitelisted() { return true }
+        BlacklistedHandleFilter blacklistedHandleFilter = new BlacklistedHandleFilter()
         blacklistedHandleFilter.metaClass.isBlacklisted() { return false }
         BlacklistedWordFilter blacklistedWordFilter = new BlacklistedWordFilter()
         blacklistedWordFilter.metaClass.isBlacklisted() { return false }
@@ -107,7 +105,72 @@ class TwitterProcessorTest extends GroovyTestCase {
         TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
         processor.processTweets(tweetList)
 
-        assert processor.filteredTweets.grayListedTweets.size() == 1
+        assert processor.filteredTweets.whiteListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.blackListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.grayListedTweets.contains(tweet)
+    }
+
+    void testTwitterProcessorFiltersTrueFalseTrue() {
+        TweetBuilder builder = new TweetBuilder()
+        Tweet tweet = builder.buildTweet()
+
+        List<Tweet> tweetList = [tweet]
+
+        WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
+        whitelistedHandleFilter.metaClass.isWhitelisted() { return true }
+        BlacklistedHandleFilter blacklistedHandleFilter = new BlacklistedHandleFilter()
+        blacklistedHandleFilter.metaClass.isBlacklisted() { return false }
+        BlacklistedWordFilter blacklistedWordFilter = new BlacklistedWordFilter()
+        blacklistedWordFilter.metaClass.isBlacklisted() { return true }
+
+        TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
+        processor.processTweets(tweetList)
+
+        assert processor.filteredTweets.whiteListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.blackListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.grayListedTweets.contains(tweet)
+    }
+
+    void testTwitterProcessorFiltersTrueTrueFalse() {
+        TweetBuilder builder = new TweetBuilder()
+        Tweet tweet = builder.buildTweet()
+
+        List<Tweet> tweetList = [tweet]
+
+        WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
+        whitelistedHandleFilter.metaClass.isWhitelisted() { return true }
+        BlacklistedHandleFilter blacklistedHandleFilter = new BlacklistedHandleFilter()
+        blacklistedHandleFilter.metaClass.isBlacklisted() { return true }
+        BlacklistedWordFilter blacklistedWordFilter = new BlacklistedWordFilter()
+        blacklistedWordFilter.metaClass.isBlacklisted() { return false }
+
+        TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
+        processor.processTweets(tweetList)
+
+        assert processor.filteredTweets.whiteListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.blackListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.grayListedTweets.contains(tweet)
+    }
+
+    void testTwitterProcessorFiltersTrueTrueTrue() {
+        TweetBuilder builder = new TweetBuilder()
+        Tweet tweet = builder.buildTweet()
+
+        List<Tweet> tweetList = [tweet]
+
+        WhitelistedHandleFilter whitelistedHandleFilter = new WhitelistedHandleFilter()
+        whitelistedHandleFilter.metaClass.isWhitelisted() { return true }
+        BlacklistedHandleFilter blacklistedHandleFilter = new BlacklistedHandleFilter()
+        blacklistedHandleFilter.metaClass.isBlacklisted() { return true }
+        BlacklistedWordFilter blacklistedWordFilter = new BlacklistedWordFilter()
+        blacklistedWordFilter.metaClass.isBlacklisted() { return true }
+
+        TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
+        processor.processTweets(tweetList)
+
+        assert processor.filteredTweets.whiteListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.blackListedTweets.contains(tweet)
+        assertFalse processor.filteredTweets.grayListedTweets.contains(tweet)
     }
 
     void testTwitterProcessorMultipleTweetsMutiplePaths() {
@@ -129,9 +192,10 @@ class TwitterProcessorTest extends GroovyTestCase {
         TwitterProcessor processor = new TwitterProcessor(whitelistedHandleFilter, blacklistedHandleFilter, blacklistedWordFilter)
         processor.processTweets(tweetList)
 
-        assert processor.filteredTweets.whiteListedTweets.size() == 1
-        assert processor.filteredTweets.blackListedTweets.size() == 2
-        assert processor.filteredTweets.grayListedTweets.size() == 1
+        assert processor.filteredTweets.whiteListedTweets.contains(tweet1)
+        assert processor.filteredTweets.blackListedTweets.contains(tweet2)
+        assert processor.filteredTweets.blackListedTweets.contains(tweet3)
+        assert processor.filteredTweets.grayListedTweets.contains(tweet4)
 
     }
 }
